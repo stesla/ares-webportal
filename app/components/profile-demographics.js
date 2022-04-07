e1c6db12a9791642a8630b1cefc44fbff2097be0
Samuel Tesla <samuel.tesla@gmail.com>
2022-04-07 02:03:20 +0000

    display swipe and missed status

--- app/components/profile-demographics.js
+++ app/components/profile-demographics.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+
+  showDatingSection: computed('dating', function() {
+    var dating = this.get('dating');
+    return dating.match || dating.swipe;
+  }),
+
 });
