dc6f11e4ed85ac6e5e2508b943b421e4f6ceea4e
Samuel Tesla <samuel.tesla@gmail.com>
2022-04-17 04:25:39 +0000

    add toggle to hide alts from match lists

--- app/controllers/dating.js
+++ app/controllers/dating.js
@@ -5,4 +5,17 @@ export default Controller.extend(Swiping, {
   reloadChar() {
     this.send('reloadModel');
   },
+
+  actions: {
+    altMatches: function(option) {
+      this.gameApi.requestOne('altMatches', { option: option }, null)
+      .then( (response) => {
+        if (response.error) {
+          return;
+        }
+        this.flashMessages.success(response.message);
+        this.reloadChar();
+      });
+    },
+  },
 });
