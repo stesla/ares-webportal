6738d1d17e8768e5b1d809d13b839e5fd5f8d06b
Samuel Tesla <samuel.tesla@gmail.com>
2022-06-03 04:53:41 +0000

    add birthday census page

---
+++ app/routes/census-birthday.js
@@ -0,0 +1,20 @@
+import EmberObject from '@ember/object';
+import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import { inject as service } from '@ember/service';
+import DefaultRoute from 'ares-webportal/mixins/default-route';
+
+export default Route.extend(DefaultRoute, {
+    gameApi: service(),
+    session: service(),
+    
+    model: function(params) {
+        let api = this.gameApi;
+        return RSVP.hash({
+             filter: params['filter'],
+             types:  api.request('censusTypes'),
+             census: api.requestOne('censusBirthday'),
+           })
+           .then((model) => EmberObject.create(model));
+    }
+});
