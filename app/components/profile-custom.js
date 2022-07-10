5c914d610ddd7dd7b5cf25d1c7d6926b5501555b
Samuel Tesla <samuel.tesla@gmail.com>
2022-07-10 19:04:54 +0000

    move match and swipe info to dating profile tab

--- app/components/profile-custom.js
+++ app/components/profile-custom.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { observer } from '@ember/object';
+import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Swiping from 'ares-webportal/mixins/swiping';
 
@@ -10,6 +10,21 @@ export default Component.extend(Swiping, {
     this._super(...arguments);
     this.updateDater();
   },
+
+  match: computed('dating.match', function() {
+    return this.get('dating.match') || 'None';
+  }),
+
+  swipe: computed('dating.swipe', function() {
+    let swipe = this.get('dating.swipe');
+    if (!swipe) {
+      return 'None';
+    } else if (swipe.missed) {
+      return swipe.type + ' (Missed Connection)';
+    } else {
+      return swipe.type;
+    }
+  }),
 
   updateDatingInfo: function(match, swipe) {
     this.set('dating.match', match);
