73e9531582898ebb095a0d2c389c382c631c73e5
Sam Tesla <stesla@pobox.com>
2023-09-13 19:26:46 -0500

    initial UI for classifieds

--- app/custom-routes.js
+++ app/custom-routes.js
@@ -2,5 +2,5 @@ export default function setupCustomRoutes(router) {
   // Define your own custom routes here, just as you would in router.js but using 'router' instead of 'this'.
   // For example:
   // router.route('yourroute');
-  
+  router.route('classifieds-ad', { path: '/classifieds-ad/:id' });
 }
---
+++ app/routes/classifieds-ad.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import DefaultRoute from 'ares-webportal/mixins/default-route';
+
+export default Route.extend(DefaultRoute, {
+    gameApi: service(),
+    session: service(),
+
+    model: function(params) {
+        let api = this.gameApi;
+        return api.requestOne('classifiedsAd', {id: params['id']});
+    }
+});
