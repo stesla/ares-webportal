9b6bac6504e09ee5ba0bc0220b4f6f766a8224d8
Samuel Tesla <samuel.tesla@gmail.com>
2023-09-22 17:48:30 -0500

    add classifieds view with columns

--- app/routes/classifieds.js
+++ app/routes/classifieds.js
@@ -7,6 +7,6 @@ export default Route.extend(DefaultRoute, {
 
     model: function(params) {
         let api = this.gameApi;
-        return api.requestOne('classifiedsAd', {id: params['id']});
+        return api.requestOne('classifieds');
     }
 });
